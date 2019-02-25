669797096acb57a46cd8d8a8dbb55c6f6e725b6b
EGLIK <eglik@odin.vision>
2019-02-25 17:16:36 +0900

    solve: programmers hash#1

---
+++ Programmers/Hash/완주하지 못한 선수.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) 
+{
+    string answer = "";
+    multiset<string> p;
+    
+    for (int i = 0; i < participant.size(); i++)
+	{
+		p.insert(participant[i]);
+	}
+
+	for (int i = 0; i < completion.size(); i++)
+	{
+		if (p.find(completion[i]) != p.end())
+		{
+            int count = p.erase(completion[i]);
+			if (count > 1)
+			{
+				for (int k = 0; k < count - 1; k++)
+				{
+					p.insert(completion[i]);
+				}
+			}
+		}
+		else
+		{
+			return completion[i];
+		}
+	}
+    
+    answer = *p.begin();
+    
+    return answer;
+}
