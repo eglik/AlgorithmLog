7c3fd71bd552623a2ca3b54f2c2c105c98f3b60c
EGLIK <eglik@odin.vision>
2019-02-26 22:27:23 +0900

    half solved: programmers hash#4
    Visual Studio 2017에서는 정답으로 나오나 웹상에서는 다른결과가 나옴. Clang으로 변경후 다시 풀것

--- programmers/Hash/베스트앨범.cpp
+++ programmers/Hash/베스트앨범.cpp
@@ -7,50 +7,54 @@
 
 using namespace std;
 
-vector<int> solution(vector<string> genres, vector<int> plays) {
+vector<int> solution(vector<string> genres, vector<int> plays) 
+{
 	vector<int> answer;
-
-	unordered_multimap<string, pair<int, int>> m;
-	map<string, int> gList;
+	
+	multimap<int, pair<string, int>, greater<int>> list;
+	unordered_multimap<string, pair<int, int>> sortList;
+	unordered_map<string, int> genreList;
 	map<int, string, greater<int>> temp;
-	unordered_map<string, int> gSortList;
-
 
 	for (int i = 0; i < genres.size(); i++)
 	{
-		m.insert(pair<string, pair<int, int>>(genres[i], pair<int, int>(plays[i], i)));
-		gList.insert(pair<string, int>(genres[i], 0));
+		list.insert(pair<int, pair<string, int>>(plays[i], pair<string, int>(genres[i], i)));
+		genreList.insert(pair<string, int>(genres[i], 0));
 	}
 
-	for (unordered_multimap<string, pair<int, int>>::iterator iter = m.begin(); iter != m.end(); iter++)
+	for (multimap<int, pair<string, int>>::iterator iter = list.begin(); iter != list.end(); iter++)
 	{
-		cout << "장르 : " << (*iter).first << ", play count :  " << (*iter).second.first << ", index : " << (*iter).second.second << endl;
+		sortList.insert(pair<string, pair<int, int>>(iter->second.first, pair<int, int>(iter->first, iter->second.second)));
 	}
 
-	// gList에 각 장르별 합을 구함
-	for (unordered_multimap<string, pair<int, int>>::iterator iter = m.begin(); iter != m.end(); iter++)
+	for (unordered_multimap<string, pair<int, int>>::iterator iter = sortList.begin(); iter != sortList.end(); iter++)
 	{
-		(*gList.find((*iter).first)).second += (*iter).second.first;
+		genreList.find(iter->first)->second += iter->second.first;
 	}
 
-	int i = 0;
-
-	cout << endl << endl;
-
-	for (map<string, int>::iterator iter = gList.begin(); iter != gList.end(); iter++)
+	for (unordered_map<string, int>::iterator iter = genreList.begin(); iter != genreList.end(); iter++)
 	{
-		temp.insert(pair<int, string>((*iter).second, (*iter).first));
+		temp.insert(pair<int, string>(iter->second, iter->first));
 	}
+
+	genreList.clear();
 
 	for (map<int, string>::iterator iter = temp.begin(); iter != temp.end(); iter++)
 	{
-		gSortList.insert(pair<string, int>((*iter).second, (*iter).first));
+		genreList.insert(pair<string, int>(iter->second, iter->first));
 	}
 
-	for (unordered_map<string, int>::iterator iter = gSortList.begin(); iter != gSortList.end(); iter++)
+	for (unordered_map<string, int>::iterator iter = genreList.begin(); iter != genreList.end(); iter++)
 	{
-		cout << "장르 : " << (*iter).first << ", 플레이횟수 : " << (*iter).second << endl;
-		// 여기서 장르별로 두곡씩 answer vector에 index를 넣어주면됨.
+		if (sortList.count(iter->first) > 1)
+		{
+			answer.push_back((sortList.equal_range((*iter).first).first)->second.second);
+			answer.push_back((++(sortList.equal_range((*iter).first).first))->second.second);
+		}
+		else if(sortList.count(iter->first) == 1)
+		{
+			answer.push_back((sortList.equal_range((*iter).first).first)->second.second);
+		}
 	}
 
 	return answer;
@@ -58,15 +62,16 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
 
 void main()
 {
-	vector<string> t1a = { "classic", "classic", "pop", "classic", "classic", "pop" };
-	vector<int> t1b = { 150, 500, 600, 150, 800, 2500 };
+	vector<string> t1a = { "classic", "pop", "classic", "classic", "pop" };
+	vector<int> t1b = { 500, 600, 150, 800, 2500 };
 	vector<int> a;
 	a = solution(t1a, t1b);
-	
+
 	cout << "[";
-	for (int i = 0; i < a.size(); i++) 
+	for (int i = 0; i < a.size() - 1; i++)
 	{
-		cout << a[i] << " ";
+		cout << a[i] << ", ";
 	}
+	cout << a[a.size() - 1];
 	cout << "]";
 }
