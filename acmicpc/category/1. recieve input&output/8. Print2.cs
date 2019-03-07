943a0c619d67df8d3f101d36d3e64f57143fb3ed
EGLIK <eglik@odin.vision>
2019-03-07 17:17:27 +0900

    solve: acmicpc category#1

---
+++ acmicpc/category/1. recieve input&output/8. Print2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
