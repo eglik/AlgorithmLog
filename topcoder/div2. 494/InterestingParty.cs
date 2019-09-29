eee7651f90bd07cfac4ae89df72522063d05cb6d
EGLIK <eglik@odin.vision>
2019-09-29 17:57:55 +0900

    solve: topcoder div2. 494#InterestingParty

---
+++ topcoder/div2. 494/InterestingParty.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+class InterestingParty
+    {
+        public static int bestInvitation(string[] first, string[] second)
+        {
+            Dictionary<string, int> bestTopics = new Dictionary<string, int>();
+
+            for(int i=0; i<first.Length; i++)
+            {
+                bestTopics[first[i]] = 0;
+                bestTopics[second[i]] = 0;
+            }
+
+            foreach (var i in first)
+            {
+                if (bestTopics.ContainsKey(i))
+                {
+                    bestTopics[i]++;
+                }
+            }
+
+            foreach (var i in second)
+            {
+                if (bestTopics.ContainsKey(i))
+                {
+                    bestTopics[i]++;
+                }
+            }
+
+            int answer = 0;
+
+            foreach (var i in bestTopics)
+            {
+                answer = Math.Max(answer, i.Value);
+            }
+
+            return answer;
+        }
+    }
