18955154fe3481a2652c6446da079aad9e58d441
weisswolfi <contact@wolfi.in>
2020-05-20 15:06:30 +0900

    solve: boj #2742

---
+++ boj/2742.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int input;
+	cin >> input;
+	for (int i = input; i > 0; i--)
+	{
+		printf("%d\n", i);
+	}
+}
