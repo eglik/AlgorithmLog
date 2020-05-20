d633153269d5e462bdc50ff4e86d84dec73a881c
weisswolfi <contact@wolfi.in>
2020-05-20 14:35:26 +0900

    solve: boj #2741

---
+++ boj/2741.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int input;
+	cin >> input;
+	for (int i = 1; i <= input; i++)
+	{
+		printf("%d\n", i);
+	}
+	return 0;
+}
