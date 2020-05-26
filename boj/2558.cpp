3e333658e3dd41999ccd009c85c3ae47f13dafaf
weisswolfi <contact@wolfi.in>
2020-05-26 10:44:18 +0900

    solve: boj #2558

---
+++ boj/2558.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+	cout << A + B << endl;
+}
