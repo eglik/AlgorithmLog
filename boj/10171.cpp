fce6f40a072baa88ada30cf40c74c16f9f8190bd
weisswolfi <contact@wolfi.in>
2020-07-03 22:18:45 +0900

    solve: boj #10171

---
+++ boj/10171.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <list>
+
+#define ll long long int
+
+using namespace std;
+
+int main() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+}
