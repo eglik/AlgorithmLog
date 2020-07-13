d54486c28fb4842ab364ebe938ad1e6a292a85ef
weisswolfi <contact@wolfi.in>
2020-07-14 00:01:02 +0900

    solve: boj #10950

---
+++ boj/10950.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+
+#define ll long long int
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	ll N;
+	cin >> N;
+	while (N--) {
+		ll A, B;
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+}
