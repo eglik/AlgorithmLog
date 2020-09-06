696ddbf131c09867ec332bacb7e810cc6f1f8883
weisswolfi <contact@wolfi.in>
2020-09-06 20:42:30 +0900

    solve: boj #18096

---
+++ boj/18096.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+
+#define ll long long int
+
+using namespace std;
+
+int main() { 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	ll N;
+	cin >> N;
+	cout << 1;
+}
