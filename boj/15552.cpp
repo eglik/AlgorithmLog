bdffb34a264d5289fb1854e2d7e5b4782ed8c508
wolfi <contact@wolfi.in>
2020-09-20 15:56:00 +0900

    solve: boj #15552

---
+++ boj/15552.cpp
@@ -0,0 +1,19 @@
+#define ll long long int
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() { 
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+    
+    ll n;
+    cin >> n;
+
+    while(n--) {
+        ll a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+}
